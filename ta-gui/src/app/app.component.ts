059bbc8aaa2c79649ea8a17bdf49381735ebed31
Carlos <crcp@cin.ufpe.br>
2022-04-04 22:39:56 -0300

    campo de login

--- ta-gui/src/app/app.component.ts
+++ ta-gui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ta-gui';
+   aluno: Aluno = {nome: "", cpf: "", email: "", login: ""};
+}
+
+export class Aluno {
+  nome: string | undefined;
+  cpf: string | undefined;
+  email: string | undefined;  
+  login: string | undefined;  
 }
